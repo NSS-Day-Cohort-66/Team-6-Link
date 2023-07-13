0489d933ea69727b8fd3ba161e2a2530d85289d0
Derek Malone <laxmalone@gmail.com>
2023-07-13 13:59:51 -0500

    completed discussing issue with having more params than needed. need to implement all the meals. Also discussed breaking out functions.

--- main.js
+++ main.js
@@ -10,10 +10,14 @@ const materials = [
 const meals = [];
 
 // TODO: We need to review and start part 2 in the readme.
+materials.push("Goat Butter", "Fresh Milk", "Cane Sugar", "Raw Bird Thigh");
 
 // function: this is for the meals being made
-const cook = (firstIngredient, secondIngredient) => {
-  // if we give two ingredients, we push a meal to the array
+
+// add 3rd and 4th ingredient as params, use them to determine if nothing was passed into the function in that slot so that we can decide what
+// meal we are going to cook.
+
+const twoIngredients = (firstIngredient, secondIngredient) => {
   if (
     firstIngredient === "Hylian Rice" &&
     secondIngredient === "Big Hearty Truffle"
@@ -39,12 +43,34 @@ const cook = (firstIngredient, secondIngredient) => {
   }
 };
 
+const cook = (
+  firstIngredient,
+  secondIngredient,
+  thirdIngredient,
+  fourthIngredient
+) => {
+  // if we give two ingredients, we push a meal to the array
+  if (thirdIngredient) {
+    if (
+      firstIngredient === "Raw Prime Meat" &&
+      secondIngredient === "Hylian Rice" &&
+      thirdIngredient === "Bird Egg"
+    ) {
+      meals.push("Chicken Egg Fried Rice");
+    }
+  } else if (fourthIngredient) {
+  } else {
+    twoIngredients(firstIngredient, secondIngredient);
+  }
+};
+
 // invoke the above function, we are passing arguments into the function
 cook(materials[0], materials[1]);
 cook(materials[4], materials[5]);
 cook(materials[2], materials[1]);
 cook(materials[2], materials[4]);
 cook(materials[3], materials[0]);
+cook(materials[3], materials[0], materials[5]);
 
 // displaying something...
 console.log("MEALS:");
